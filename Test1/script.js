7fbbc3b791dea471a126f28fed5a3dd3556044ec
Farha Sayed <sc21frs@leeds.ac.uk>
2024-12-09 20:25:12 +0400

    observability

---
+++ Test1/script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+
+    // the two elements we target
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+  
+    // the action on those elements
+    // basically, when we click hamburger, it selects the open functions
+    // when we close hamburger, it selects the regular fns
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+  }
+
+
+
+
