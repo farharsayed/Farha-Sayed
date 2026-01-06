3b36f600e47b1301f8193b65c55bd77eac60b2da
Farha Sayed <farhasayed29@gmail.com>
2026-01-06 12:02:49 +0400

    latest

---
+++ Devops/Script-Test/2-function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def greeting(fname, lname, country):
+    print("Hello, %s %s! How is the weather in %s?" % (fname, lname, country))
+
+first_name = "Farha"
+last_name = "Sayed"
+mycountry = "UAE"
+
+greeting(first_name, last_name, mycountry)
